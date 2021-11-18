e1b6aa74d52e0987886e42be30f9327bdb2cf634
Chintan Mak <55547993+chintanmak@users.noreply.github.com>
2021-11-18 14:48:35 +0530

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title>HELLO1</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="topnav">
+  <a href="hello.html">Home</a>
+  <a href="about.html">About</a>
+  <a href="challenge.html">Challenge</a>
+</div>
+
+<h1><center>HELLOOOO</center></h1>
+
+<br>
+<div class="maininput">
+	<input type="text" cols="50" name="search" placeholder="Search here..!!">
+	<input type="submit" name="search" value="search">
+</div>
+
+
+</body>
+</html>
