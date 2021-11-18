c716a6b0574b9fb79996bde8305ea1fabf4c5d5a
Chintan Mak <55547993+chintanmak@users.noreply.github.com>
2021-11-18 15:50:32 +0530

    Add files via upload

--- hello.php
+++ hello.php
@@ -9,18 +9,57 @@
   <a href="hello.html">Home</a>
   <a href="about.html">About</a>
   <a class="active" href="hello.php">Challenge</a>
-  <a href="keyword.html">Keyword</a>
+  <a href="keyword.php">Keyword</a>
+
 </div>
 
-<form action="" method="POST">
+
+<form action="hello.php" method="POST">
 
 <div class="maininput">
-	<input type="text" cols="50" name="search" placeholder="Search here..!!">
+	<input type="text" name="name" placeholder="Search here..!!">
 	<input type="submit" name="search" value="search">
 </div>
 	
 </form>
 
+<table border="2">
+  <tr>
+    <td>ID</td>
+    <td>Hint</td>
+  </tr>
+
+<?php
+
+$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "database1";
+
+
+	$db = new mysqli($servername, $username, $password, $dbname);
+
+	if ($db->connect_error) {
+		die("Connection failed: " . $db->connect_error);
+	}
+
+	$uname = $_POST["name"];
+
+$records = mysqli_query($db,"SELECT * FROM hints WHERE name='$uname'"); // fetch data from database
+
+while($data = mysqli_fetch_array($records))
+{
+?>
+  <tr>
+    <td><?php echo $data['id']; ?></td>
+    <td><?php echo $data['name']; ?></td>
+  </tr>	
+<?php
+}
+?>
+</table>
+
+<?php mysqli_close($db); // Close connection ?>
 
 </body>
 </html>
---
+++ keyword.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>HELLO1</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="topnav">
+  <a href="hello.html">Home</a>
+  <a href="about.html">About</a>
+  <a href="hello.php">Challenge</a>
+  <a class="active" href="keyword.php">Keyword</a>
+</div>
+
+<br>
+
+<center><h2>Enter the Keyword here..</h2>
+<h4>The hint is **** ****</h4>
+</center>
+
+
+<form action="keyword.php" method="POST">
+
+<div class="maininput">
+	<input type="text" name="name" placeholder="Search here..!!">
+	<input type="submit" name="search" value="search">
+</div>
+	
+</form>
+
+</body>
+</html>
